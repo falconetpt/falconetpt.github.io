85dd7b37796edfca7762f6358a16297afe71f32f
falconetpt <ipsiart@gmail.com>
2016-09-29 12:45:51 +0000

    testes

--- isep/aprog/js/script.js
+++ isep/aprog/js/script.js
@@ -1,13 +1,10 @@
 $( document ).ready(function() {
   // MVC object model
   $.jStorage.set("tests", "Hello",  "https://falconetpt.github.io/isep/aprog/data/exercises.txt");
-  
-  var value = $.jStorage.index()
-  
-  console.log(value);
-  
   //Funçao acordiao
   $( function() {
       $( "#accordion" ).accordion();
   } );
+  var value = $.jStorage.index()
+  console.log(value);
 });
