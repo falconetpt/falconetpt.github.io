5d96c9024a4c03d0fe8570e959bee8b0bc911518
Ricardo Gomes <ipsiart@gmail.com>
2016-09-29 11:35:05 +0000

    testes

--- isep/aprog/js/script.js
+++ isep/aprog/js/script.js
@@ -2,11 +2,28 @@ $( document ).ready(function() {
   // MVC object model
 
     var mvc = {
-        exercises: []
+        reader: function readTextFile(filePath)
+            {
+                var rawFile = new XMLHttpRequest();
+                rawFile.open("GET", filePath, true);
+                rawFile.onreadystatechange = function ()
+                {
+                    if(rawFile.readyState === 4)
+                    {
+                        var allText = rawFile.responseText;
+                        $("#accordion").text(allText);
+                    }
+                }
+            
+                rawFile.send();
+            }
     };
     
     //Funçao acordiao
     $( function() {
         $( "#accordion" ).accordion();
     } );
+    
+    //Ler file txt
+    mvc.reader(../data/exercises.txt);
 });
