32f75cf5e815a1bde17f5d6c0b2fdfb519c6583c
Ricardo Gomes <ipsiart@gmail.com>
2016-09-29 12:34:10 +0000

    testes

--- isep/aprog/js/script.js
+++ isep/aprog/js/script.js
@@ -1,34 +1,8 @@
 $( document ).ready(function() {
   // MVC object model
-
-    var mvc = {
-        reader: function readTextFile(filePath)
-            {
-                var rawFile = new XMLHttpRequest();
-                rawFile.open("GET", filePath, true);
-                rawFile.onreadystatechange = function ()
-                {
-                    if(rawFile.readyState === 4)
-                    {
-                        var allText = rawFile.responseText;
-                        $("#accordion").html(allText);
-                    }
-                }
-            
-                rawFile.send();
-            },
-    };
-      
-    $.getJSON("https://falconetpt.github.io/isep/aprog/data/exercises.json", function(data) {
-        var outputHTML = "";
-        for(var i = 0; i < data.length; i++) {
-          outputHTML += data[i];
-        }
-        $('#accordion').html(outputHTML)
-    });
-    
-    //Funçao acordiao
-    $( function() {
-        $( "#accordion" ).accordion();
-    } );
+  
+  //Funçao acordiao
+  $( function() {
+      $( "#accordion" ).accordion();
+  } );
 });
