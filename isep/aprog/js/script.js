aab505933ae450ced6756a63d8f3223840dd055f
falconetpt <ipsiart@gmail.com>
2016-09-30 08:32:35 +0000

    added mvc in js for the rendering

--- isep/aprog/js/script.js
+++ isep/aprog/js/script.js
@@ -1,7 +1,35 @@
 $( document ).ready(function() {
   // MVC object model
-
-  //Funçao acordiao
+  var mvc = {
+    struture: [["1", "data/APROG_1151888_1160766_ex1.txt"], ["2", "data/APROG_1151888_1160766_ex1.txt"]],
+    init: function() {
+      mvc.render();
+    },
+    render: function () {
+      var htmlStr = "";
+      
+      for (var i = 0, n = mvc.struture.length; i < n; i++) {
+        htmlStr += "<h3>Exercicio " + mvc.struture[i][0]  + "</h3>";
+        htmlStr +=  "<div>";
+        htmlStr +=    "<p>"
+        htmlStr +=    "<div>";
+        htmlStr +=    "<p>";
+        htmlStr +=      "<iframe src=\"" + mvc.struture[i][1] + "\" width=200 height=400 frameborder=0 >";
+        htmlStr +=      "</iframe>";
+        htmlStr +=    "</p>";
+        htmlStr +=    "</div>";
+        htmlStr +=    "</p>";
+        htmlStr +=  "</div>";
+      }
+      
+      $("#accordion").append(htmlStr);
+    }
+  };
+  
+  //initializes the rendering functions
+  mvc.init();
+  
+  //Acordion Function
   $( function() {
       $( "#accordion" ).accordion();
   } );
