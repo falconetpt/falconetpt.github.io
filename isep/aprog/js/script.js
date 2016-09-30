e364c59481c710e72878e3b95394d3ce32106a98
falconetpt <ipsiart@gmail.com>
2016-10-01 00:23:21 +0100

    Update script.js

--- isep/aprog/js/script.js
+++ isep/aprog/js/script.js
@@ -1,27 +1,17 @@
 $( document ).ready(function() {
   // MVC object model
   var mvc = {
-    struture: [/*["1B", "data/1B.txt"], 
+    struture: [["1B", "data/1B.txt"], 
       ["3", "data/3.txt"], 
       ["4", "data/4.txt"],
       ["5", "data/5.txt"],
-      ["6", "data/6.txt"]*/],
+      ["6", "data/6.txt"]],
     init: function() {
       console.log(mvc.getFolder("data"));
       mvc.render();
     },
     getFolder: function (dir) {
-        fileList = [];
-        var fs = new ActiveXObject("Scripting.FileSystemObject");
-        var files = fs.readdirSync(dir);
-        for(var i in files){
-            if (!files.hasOwnProperty(i)) continue;
-            var name = dir+'/'+files[i];
-            if (!fs.statSync(name).isDirectory()){
-                fileList.push(name);
-            }   
-        }
-        return fileList;
+      
     },
     render: function () {
       var htmlStr = "";
