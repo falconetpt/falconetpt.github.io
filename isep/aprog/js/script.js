3f50b6719ec6bfd309841e6ef2145197c4337750
falconetpt <ipsiart@gmail.com>
2016-10-03 11:38:50 +0100

    Update script.js

--- isep/aprog/js/script.js
+++ isep/aprog/js/script.js
@@ -31,6 +31,7 @@ $( document ).ready(function() {
         htmlStr +=    "<p>";
         htmlStr +=      "<iframe src=\"" + mvc.struture[i][1] + "\" frameborder=0 width='100%' height='100%'>";
         htmlStr +=      "</iframe>";
+        htmlStr +=      "<hr>"
         htmlStr +=    "</p>";
         htmlStr +=    "</div>";
         htmlStr +=    "</p>";
