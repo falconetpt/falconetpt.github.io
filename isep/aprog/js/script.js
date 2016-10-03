3ce549fd823427a5690bbf643a7373032a7a4340
falconetpt <ipsiart@gmail.com>
2016-10-03 17:04:41 +0100

    Update script.js

--- isep/aprog/js/script.js
+++ isep/aprog/js/script.js
@@ -75,14 +75,12 @@ $( document ).ready(function() {
         htmlStr +=    "<p>";
         htmlStr +=      "<iframe src=\"" + mvc.struture[i][1] + "\" frameborder=0 width='100%' height='100%'>";
         htmlStr +=      "</iframe>";
+        htmlStr +=    "</p><hr>";
+	/*     
 	//evaluates if code has sample code file and fetch that text
 	if(mvc.struture[i].length > 2) {
 		htmlStr += 	  "<pre><code class='java'>" + mvc.sampleCode[mvc.struture[i][2]] + "</code></pre>"
-	}
-        htmlStr +=    "</p><hr>";
-		
-
-		
+	}	*/
         htmlStr +=    "</div>";
         htmlStr +=    "</p>";
 		
