65d503e68d46c08bbcfbac4a4b11fcf908c45cdb
falconetpt <ipsiart@gmail.com>
2016-10-07 19:32:21 +0100

    Update script.js

--- isep/aprog/js/script.js
+++ isep/aprog/js/script.js
@@ -16,8 +16,7 @@ $( document ).ready(function() {
       ["8A", "data/8A.txt"],
       ["8B", "data/8B.txt"],
       ["8C", "data/8C.txt"],
-      ["9", "data/9.txt"],
-      ["10", "data/10.txt"],],
+      ["9", "data/9.txt"],],
     init: function() {
       mvc.render();
     },
